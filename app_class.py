006be915f5deec1962e9787347c7945dc4f64ff7
Știrbu Denisa-Elena <48128844+DenisaElena99@users.noreply.github.com>
2020-05-04 20:55:02 +0300

    Status: work in progress; maze background + player

---
+++ app_class.py
@@ -0,0 +1,114 @@
+import pygame
+import sys
+import copy
+from settings import *
+from player_class import *
+
+
+pygame.init()
+vec = pygame.math.Vector2
+logo_surface = pygame.display.set_mode((20, 20))
+
+class App:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.state = 'start'
+        self.cell_width = MAZE_WIDTH//28
+        self.cell_height = MAZE_HEIGHT//30
+        self.player = Player(self, PLAYER_START_POS)
+        self.load()
+
+
+    def run(self):
+        while self.running:
+            if self.state == 'start':
+                self.start_events()
+                self.start_update()
+                self.start_draw()
+            elif self.state == 'playing':
+                self.playing_events()
+                self.playing_update()
+                self.playing_draw()
+            else:
+                self.running = False
+            self.clock.tick(FPS)
+        pygame.quit()
+        sys.exit()
+
+# HELPER FUNCTIONS #
+    def draw_text(self, words, screen, pos, size, colour, font_name, centered = False):
+        font = pygame.font.SysFont(font_name, size)
+        text = font.render(words, False, colour)
+        text_size = text.get_size()
+        if centered:
+            pos[0] = pos[0] - text_size[0]//2
+            pos[1] = pos[1] - text_size[1]//2
+
+        screen.blit(text, pos)
+
+    def load(self):
+        self.background = pygame.image.load('maze.png')
+        self.background = pygame.transform.scale(self.background, (MAZE_WIDTH,
+        MAZE_HEIGHT))
+
+    def draw_grid(self):
+        for x in range(WIDTH//self.cell_width):
+            pygame.draw.line(self.background, GREY, (x*self.cell_width, 0), (x*self.cell_width, HEIGHT))
+        for x in range(HEIGHT//self.cell_height):
+            pygame.draw.line(self.background, GREY, (0, x*self.cell_height), (WIDTH, x*self.cell_height))
+
+
+# INTRO FUNCTIONS #
+
+    def start_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.state = 'playing'
+
+    def start_update(self):
+        pass
+
+    def start_draw(self):
+
+        self.screen.fill(BLACK)
+        self.draw_text('HIGH SCORE', self.screen, [4, 0], START_TEXT_SIZE, WHITE, START_FONT)
+        self.draw_text('PUSH SPACE BAR TO START', self.screen, [WIDTH//2, HEIGHT//2+50], START_TEXT_SIZE, WHITE, START_FONT, True)
+        self.draw_text('1 PLAYER ONLY', self.screen, [WIDTH//2, HEIGHT//2+100], START_TEXT_SIZE, MINT, START_FONT, True)
+        self.draw_text('© 2020 BICICLETE.RO', self.screen, [WIDTH//2, HEIGHT//2+150], START_TEXT_SIZE, BLUE, START_FONT, True)
+        pygame.display.set_caption('Pac-Bike')
+        logo = pygame.image.load('logo.png')
+        wlogo = logo.get_width()
+        logo_surface.blit(logo, (WIDTH//2-wlogo//2, 85))
+        pygame.display.update()
+
+# PLAYING FUNCTIONS #
+
+    def playing_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.player.move(vec(-1,0))
+                if event.key == pygame.K_RIGHT:
+                    self.player.move(vec(1, 0))
+                if event.key == pygame.K_UP:
+                    self.player.move(vec(0, -1))
+                if event.key == pygame.K_DOWN:
+                    self.player.move(vec(0, 1))
+
+    def playing_update(self):
+        self.player.update()
+
+    def playing_draw(self):
+        self.screen.fill(BLACK)
+        self.screen.blit(self.background, (TOP_BUTTOM_BUFFER//2, TOP_BUTTOM_BUFFER//2))
+        self.draw_grid()
+        self.draw_text('CURRENT SCORE: 0', self.screen, [60,0], 18, WHITE, START_FONT)
+        self.draw_text('HIGH SCORE: 0', self.screen, [WIDTH//2 + 60, 0], 18, WHITE, START_FONT)
+        self.player.draw()
+        pygame.display.update()
---
+++ settings.py
@@ -0,0 +1,25 @@
+from pygame.math import Vector2 as vec
+
+# screen settings
+WIDTH, HEIGHT = 610, 670
+TOP_BUTTOM_BUFFER = 50
+MAZE_WIDTH, MAZE_HEIGHT = WIDTH - TOP_BUTTOM_BUFFER, HEIGHT - TOP_BUTTOM_BUFFER
+FPS = 60
+
+
+# colour settings
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+MINT = (173, 239, 209)
+BLUE = (0, 99, 178)
+GREY = (107, 107, 107)
+PLAYER_COLOUR = (190, 194, 15)
+RED = (255, 0, 0)
+# font settings
+START_TEXT_SIZE = 15
+START_FONT = 'arial black'
+
+# player settings
+PLAYER_START_POS = vec(1,1)
+
+# mod settings
